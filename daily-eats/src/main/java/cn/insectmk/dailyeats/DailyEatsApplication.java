26bdf2247a15d72f3aa8a7e118d9f78a2de9e09b
makuning <makun_ing@126.com>
2024-10-09 16:42:00 +0800

    初始化项目，完成数据库的创建

---
+++ daily-eats/src/main/java/cn/insectmk/dailyeats/DailyEatsApplication.java
@@ -0,0 +1,13 @@
+package cn.insectmk.dailyeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyEatsApplication.class, args);
+    }
+
+}
---
+++ daily-eats/src/test/java/cn/insectmk/dailyeats/DailyEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.insectmk.dailyeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyEatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
