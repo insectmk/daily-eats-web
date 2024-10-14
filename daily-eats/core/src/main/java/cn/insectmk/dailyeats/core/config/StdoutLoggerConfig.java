65433bcc56cb027daf577a445d94f0c625ffd9e3
makuning <makun_ing@126.com>
2024-10-14 17:53:30 +0800

    使用log4j-2替换SpringBoot自带的Logger

--- daily-eats/core/src/main/java/cn/insectmk/dailyeats/core/config/StdoutLoggerConfig.java
+++ daily-eats/core/src/main/java/cn/insectmk/dailyeats/core/config/StdoutLoggerConfig.java
@@ -1,13 +1,17 @@
 package cn.insectmk.dailyeats.core.config;
 
+import com.p6spy.engine.spy.appender.StdoutLogger;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @Description 日志打印配置
  * @Author makun
  * @Date 2024/10/12 17:50
  * @Version 1.0
  */
-public class StdoutLogger extends com.p6spy.engine.spy.appender.StdoutLogger {
+@Slf4j
+public class StdoutLoggerConfig extends StdoutLogger {
     public void logText(String text) {
-        System.err.println(text);
+        log.info(text);
     }
 }
