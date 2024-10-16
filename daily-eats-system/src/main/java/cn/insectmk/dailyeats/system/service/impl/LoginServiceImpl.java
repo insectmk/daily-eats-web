5bb7bc79b8a4cfb384108bdfbda10c3705fcbbec
makuning <makun_ing@126.com>
2024-10-16 11:12:03 +0800

    增加登录接口

--- daily-eats-system/src/main/java/cn/insectmk/dailyeats/system/service/impl/LoginServiceImpl.java
+++ daily-eats-system/src/main/java/cn/insectmk/dailyeats/system/service/impl/LoginServiceImpl.java
@@ -1,11 +1,17 @@
 package cn.insectmk.dailyeats.system.service.impl;
 
+import cn.insectmk.dailyeats.common.enums.ExceptionCodeEnum;
+import cn.insectmk.dailyeats.common.exception.ServiceException;
 import cn.insectmk.dailyeats.system.domain.dto.LoginUserDto;
+import cn.insectmk.dailyeats.system.domain.entity.User;
 import cn.insectmk.dailyeats.system.service.IUserService;
 import cn.insectmk.dailyeats.system.service.LoginService;
 import cn.insectmk.dailyeats.system.service.TokenService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description 登录服务接口实现
@@ -24,12 +30,21 @@ public class LoginServiceImpl implements LoginService {
     /**
      * 用户登录
      * @param loginUserDto 登录信息
-     * @return
+     * @return Jwt令牌
      */
     @Override
     public String login(LoginUserDto loginUserDto) {
         // 判断信息是否正确
+        User loginUser = userService.getOne(new LambdaQueryWrapper<User>()
+                .eq(User::getUsername, loginUserDto.getUsername())
+                .eq(User::getPassword, loginUserDto.getPassword()));
+        if (loginUser == null) {
+            throw new ServiceException("用户名或密码错误", ExceptionCodeEnum.FAIL_USER.getCode());
+        }
         // 生成token
-        return tokenService.getToken(null);
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", loginUser.getId());
+        claims.put("username", loginUserDto.getUsername());
+        return tokenService.getToken(claims);
     }
 }
