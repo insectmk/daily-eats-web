c7811e152c01868b29269f1bd1947ea5c0add32d
makuning <makun_ing@126.com>
2024-10-26 15:29:25 +0800

    更新字段名

--- database/update/tables.sql
+++ database/update/tables.sql
@@ -9,10 +9,10 @@ CREATE TABLE IF NOT EXISTS `t_user` (
     `height` float DEFAULT NULL COMMENT '身高',
     `weight` float DEFAULT NULL COMMENT '体重',
     `gender` char(1) DEFAULT NULL COMMENT '性别',
-    `input_by` varchar(32) DEFAULT NULL COMMENT '录入人',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
-    `modify_by` varchar(32) DEFAULT NULL COMMENT '修改人',
-    `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
     `memo` varchar(2000) DEFAULT NULL COMMENT '备注',
     CONSTRAINT `pk_t_user_id` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表，储存用户信息';
@@ -29,10 +29,10 @@ CREATE TABLE IF NOT EXISTS `t_recipe` (
     `level` tinyint DEFAULT '0' COMMENT '烹饪难度',
     `sort` int DEFAULT '0' COMMENT '排序',
     `is_pub` tinyint(1) DEFAULT '0' COMMENT '是否公开',
-    `input_by` varchar(32) DEFAULT NULL COMMENT '录入人',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
-    `modify_by` varchar(32) DEFAULT NULL COMMENT '修改人',
-    `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
     `memo` varchar(2000) DEFAULT NULL COMMENT '备注',
     CONSTRAINT `pk_t_recipe_id` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜谱表，储存菜谱信息';
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `t_user_recipe` (
     `recipe_id` varchar(32) DEFAULT NULL COMMENT '菜谱ID',
     `memo` varchar(2000) DEFAULT NULL COMMENT '备注',
     `sort` int DEFAULT NULL COMMENT '排序',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
     CONSTRAINT `pk_t_user_recipe_id` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户菜谱表，储存用户的菜谱信息';
 
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `t_user_recipe_tag` (
     `user_id` varchar(32) NOT NULL COMMENT '用户ID',
     `recipe_id` varchar(32) NOT NULL COMMENT '菜谱ID',
     `name` varchar(20) DEFAULT NULL COMMENT '名称',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
     CONSTRAINT `pk_t_user_recipe_tag_id` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户菜谱标签表，储存用户菜谱标签';
 
@@ -64,10 +64,10 @@ CREATE TABLE IF NOT EXISTS `t_food` (
     `name` varchar(20) DEFAULT NULL COMMENT '名称',
     `type` varchar(20) DEFAULT NULL COMMENT '分类',
     `unit` varchar(10) DEFAULT NULL COMMENT '单位',
-    `input_by` varchar(32) DEFAULT NULL COMMENT '录入人',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
-    `modify_by` varchar(32) DEFAULT NULL COMMENT '修改人',
-    `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
     `memo` varchar(2000) DEFAULT NULL COMMENT '备注',
     CONSTRAINT `pk_t_food_id` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='食材表，储存原料信息';
@@ -77,10 +77,10 @@ CREATE TABLE IF NOT EXISTS `t_food_price` (
     `id` varchar(32) NOT NULL COMMENT '主键',
     `food_id` varchar(32) DEFAULT NULL COMMENT '食材ID',
     `price` decimal(10,2) DEFAULT NULL COMMENT '单价',
-    `input_by` varchar(32) DEFAULT NULL COMMENT '录入人',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
-    `modify_by` varchar(32) DEFAULT NULL COMMENT '修改人',
-    `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
     `memo` varchar(2000) DEFAULT NULL COMMENT '备注',
     CONSTRAINT `pk_t_food_price_id` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='食材价格表，储存价格信息';
@@ -91,10 +91,10 @@ CREATE TABLE IF NOT EXISTS `t_recipe_food` (
     `recipe_id` varchar(32) DEFAULT NULL COMMENT '菜谱ID',
     `food_id` varchar(32) DEFAULT NULL COMMENT '食材ID',
     `amount` float DEFAULT NULL COMMENT '量',
-    `input_by` varchar(32) DEFAULT NULL COMMENT '录入人',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
-    `modify_by` varchar(32) DEFAULT NULL COMMENT '修改人',
-    `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
     `memo` varchar(2000) DEFAULT NULL COMMENT '备注',
     CONSTRAINT `pk_t_recipe_food_id` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜谱食材表，储存菜谱的食材信息';
@@ -104,10 +104,10 @@ CREATE TABLE IF NOT EXISTS `t_recipe_cook_tool` (
     `id` varchar(32) NOT NULL COMMENT '主键',
     `name` varchar(50) DEFAULT NULL COMMENT '名称',
     `type` varchar(20) DEFAULT NULL COMMENT '类型',
-    `input_by` varchar(32) DEFAULT NULL COMMENT '录入人',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
-    `modify_by` varchar(32) DEFAULT NULL COMMENT '修改人',
-    `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
     `memo` varchar(2000) DEFAULT NULL COMMENT '备注',
     CONSTRAINT `pk_t_recipe_cook_tool_id` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜谱烹饪工具表，储存菜谱烹饪工具信息';
@@ -118,10 +118,10 @@ CREATE TABLE IF NOT EXISTS `t_role` (
     `name` varchar(50) DEFAULT NULL COMMENT '名称',
     `sort` int default 0 null COMMENT '排序',
     `status` char(1)  null default '1' COMMENT '角色状态（1正常 0停用）',
-    `input_by` varchar(32) DEFAULT NULL COMMENT '录入人',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
-    `modify_by` varchar(32) DEFAULT NULL COMMENT '修改人',
-    `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
     `memo` varchar(2000) DEFAULT NULL COMMENT '备注',
     `is_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '逻辑删除标志',
     CONSTRAINT `pk_t_role_id` PRIMARY KEY (`id`)
@@ -136,10 +136,10 @@ CREATE TABLE IF NOT EXISTS `t_menu` (
     `status` char(1) default '1' comment '菜单状态（0正常 1停用）',
     `icon` varchar(100) default '#' comment '菜单图标',
     `sort` int default 0 null COMMENT '排序',
-    `input_by` varchar(32) DEFAULT NULL COMMENT '录入人',
-    `input_time` datetime DEFAULT NULL COMMENT '录入时间',
-    `modify_by` varchar(32) DEFAULT NULL COMMENT '修改人',
-    `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
     `memo` varchar(2000) DEFAULT NULL COMMENT '备注',
     `is_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '逻辑删除标志',
     CONSTRAINT `pk_t_menu_id` PRIMARY KEY (`id`)
