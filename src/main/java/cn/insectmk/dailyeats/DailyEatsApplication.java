8ea86f2bff6dbe6a42aadfa7d99082fe19e5c852
makuning <makun_ing@126.com>
2024-10-28 10:02:24 +0800

    重构项目为单模块项目

--- src/main/java/cn/insectmk/dailyeats/DailyEatsApplication.java
+++ src/main/java/cn/insectmk/dailyeats/DailyEatsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan(basePackages = {
-        "cn.insectmk.dailyeats.mapper",
         "cn.insectmk.dailyeats.mapper"})
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "每日饭菜", description = "管理自己的每日三餐", version = "1.0.0"))
