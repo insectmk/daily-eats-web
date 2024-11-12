c1b4207f45cdb1d1251798d2d4eb58b981c05e60
makuning <makun_ing@126.com>
2024-11-12 10:21:09 +0800

    MybatisPlus增加分页

---
+++ src/main/java/cn/insectmk/dailyeats/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package cn.insectmk.dailyeats.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description MybatisPlus配置
+ * @Author makun
+ * @Date 2024/11/12 10:16
+ * @Version 1.0
+ */
+@Configuration
+@MapperScan("cn.insectmk.dailyeats.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 添加分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
+        // 如果有多数据源可以不配具体类型, 否则都建议配上具体的 DbType
+        return interceptor;
+    }
+}
--- src/test/java/cn/insectmk/dailyeats/service/impl/UserServiceImplTest.java
+++ src/test/java/cn/insectmk/dailyeats/service/impl/UserServiceImplTest.java
@@ -2,6 +2,8 @@ package cn.insectmk.dailyeats.service.impl;
 
 import cn.insectmk.dailyeats.domain.entity.User;
 import cn.insectmk.dailyeats.service.IUserService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +14,13 @@ import java.util.List;
 class UserServiceImplTest {
     @Autowired
     private IUserService userService;
+
+    @Test
+    void getUserPage() {
+        IPage<User> page = new Page<>(1, 10);
+        userService.page(page);
+        System.out.println(page.getRecords());
+    }
 
     @Test
     void getUser() {
